5492652f5f0d198434dc3ffa4472b793f96c0a94
jokhatab <137109520+jokhatab@users.noreply.github.com>
2023-08-08 01:58:44 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+// //////////////////////////
+var copy =document.querySelector("#copy")
+var comment =document.querySelector("#comment")
+copy.onclick =function(){
+    confirm("copied")
+
+}
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+  });
